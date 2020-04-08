6275bbf0b98e6719146da5c39c90690ca147d54e
okuninoshi <gennesis@outlook.fr>
2020-04-08 20:47:32 +0500

    Updates

---
+++ precache-manifest.46b44222e8be444b5a94c0f79920d24a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71a2dac3a2396d2d1886133aff03135",
+    "url": "/gyroscope/index.html"
+  },
+  {
+    "revision": "aad28e994aa358213566",
+    "url": "/gyroscope/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c32e838dba4c25d6e684",
+    "url": "/gyroscope/static/js/2.3ec0d3b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gyroscope/static/js/2.3ec0d3b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad28e994aa358213566",
+    "url": "/gyroscope/static/js/main.5d8f8a12.chunk.js"
+  },
+  {
+    "revision": "4ee9e17d887809ecdfea",
+    "url": "/gyroscope/static/js/runtime-main.b492486d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gyroscope/static/media/logo.5d5d9eef.svg"
+  }
+]);
